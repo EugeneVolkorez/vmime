a05e509a2ec7004d8c10fdf435a18cb8c3df16bb
eugene <evgeny.volkorez@duallab.com>
2014-01-23 15:52:32 +0300

    ccmip: win: add build scripts

---
+++ ccmip-make.bat
@@ -0,0 +1,18 @@
+setlocal
+
+::cd "%CD%\build"
+
+CALL "%VS120COMNTOOLS%\vsvars32.bat" > nul
+msbuild vmime.sln /p:Configuration=Release || goto :error
+msbuild INSTALL.vcxproj /p:Configuration=Release || goto :error
+
+msbuild vmime.sln /p:Configuration=Debug || goto :error
+msbuild INSTALL.vcxproj /p:Configuration=Debug || goto :error
+
+
+echo Succeed
+goto :EOF
+
+:error
+echo Failed
+exit /b %errorlevel%
