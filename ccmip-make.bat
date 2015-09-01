2ec22f54f2861a4bae9cce42a076f787402b51bc
eugene <evgeny.volkorez@duallab.com>
2015-09-01 14:13:03 +0300

    move to vs2012

--- ccmip-make.bat
+++ ccmip-make.bat
@@ -2,12 +2,12 @@ setlocal
 
 cd "%CD%\build"
 
-CALL "%VS90COMNTOOLS%\vsvars32.bat" > nul
+CALL "%VS120COMNTOOLS%\vsvars32.bat" > nul
 msbuild vmime.sln /p:Configuration=Release || goto :error
-msbuild INSTALL.vcproj /p:Configuration=Release || goto :error
+msbuild INSTALL.vcxproj /p:Configuration=Release || goto :error
 
 msbuild vmime.sln /p:Configuration=Debug || goto :error
-msbuild INSTALL.vcproj /p:Configuration=Debug || goto :error
+msbuild INSTALL.vcxproj /p:Configuration=Debug || goto :error
 
 
 echo Succeed
